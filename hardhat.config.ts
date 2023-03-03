0de32465e4a7729ad99608e45ab9bed438377da1
catching snowflakes <toptal126@gmail.com>
2023-03-03 18:00:32 +0900

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://rpc.ankr.com/bsc",
+      },
+    },
+  },
+};
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+export default config;
